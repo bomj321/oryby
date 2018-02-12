d48b9735a5b8cbb5350e41007c746628ff597cd5
Jose Ortega <mauricio1314@hotmail.com>
2018-02-12 05:00:41 -0400

    Merge branch 'master' of https://github.com/bomj321/oryby

--- help.php
+++ help.php
@@ -6,15 +6,15 @@ include('head.php');
 <html>
 
     <body>
-     <!-- start topBar --> 
-     <?php include('topbar.php');  
+     <!-- start topBar -->
+     <?php include('topbar.php');
     include('middlebar.php');
-    
+
 	 ?>
 	 <!-- end topBar -->
-        
+
        <?php include('navh.php'); ?>
-        
+
         <!-- start section -->
         <section class="section white-backgorund">
             <div class="container">
@@ -22,9 +22,9 @@ include('head.php');
                     <div class="col-sm-9">
                         <h2 class="title">Help</h2>
                         <p>Aliquam posuere erat et orci eleifend cursus. Nullam tempus odio sem, lacinia pellentesque neque mollis a. In ut tempor ligula. Vestibulum ultricies bibendum lorem, a sollicitudin tellus euismod vel. Nam suscipit, diam ut volutpat lobortis, nibh ipsum tempor nibh, a vehicula tellus justo id nibh. Nulla pretium mollis convallis. Phasellus a nibh aliquet, ullamcorper quam aliquam, faucibus nulla. Phasellus mollis tristique vehicula. Vivamus sagittis, sem sed posuere aliquet, massa odio lobortis.</p>
-                        
+
                         <hr class="spacer-20">
-                        
+
                         <div class="row">
                             <div class="col-sm-4">
                                 <h6>Lorem Ipsum</h6>
@@ -40,17 +40,17 @@ include('head.php');
                             </div><!-- end col -->
                         </div><!-- end row -->
                     </div><!-- end col -->
-                    
-                </div><!-- end row -->                
+
+                </div><!-- end row -->
             </div><!-- end container -->
         </section>
         <!-- end section -->
-               
+
         <!-- start footer -->
         <?php require 'footer.php'; ?>
         <!-- end footer -->
-        
-        
+
+
         <!-- JavaScript Files -->
         <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
         <script type="text/javascript" src="js/bootstrap.min.js"></script>
@@ -65,6 +65,6 @@ include('head.php');
         <script type="text/javascript" src="js/gmaps.js"></script>
         <script type="text/javascript" src="js/swiper.min.js"></script>
         <script type="text/javascript" src="js/main.js"></script>
-        
+
     </body>
 </html>
