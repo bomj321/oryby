1edffa5b22677a3b29955c737114f425ac91a163
Alemarnery <aleemar.95@gmail.com>
2018-02-13 19:48:14 -0400

    logo de la pagina en español

--- spanish/b2b/breq1.php
+++ spanish/b2b/breq1.php
@@ -7,61 +7,17 @@ require 'Connect.php';
  $keyword = $_POST['pais'];
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Plus - E-Commerce Template</title>
-    <meta charset="utf-8">
-    <meta name="description" content="Plus E-Commerce Template">
-    <meta name="author" content="Diamant Gjota" />
-    <meta name="keywords" content="plus, html5, css3, template, ecommerce, e-commerce, bootstrap, responsive, creative" />
-    <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
+<?php
+include('head.php');
+?>
 
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-
-    <!--Favicon-->
-    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
-    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
-
-    <!-- css files -->
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
-
-    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" />
-    <link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css" />
-
-    <link rel="stylesheet" type="text/css" href="css/owl.theme.default.min.css" />
-    <link rel="stylesheet" type="text/css" href="css/animate.css" />
-    <link rel="stylesheet" type="text/css" href="css/swiper.css" />
-
-    <!-- this is default skin you can replace that with: dark.css, yellow.css, red.css ect -->
-    <link id="pagestyle" rel="stylesheet" type="text/css" href="css/default.css" />
-
-    <!-- Google fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900,900i&subset=cyrillic,cyrillic-ext,latin-ext" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:100,300,400,700" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Dosis:200,300,400,500,600,700,800&amp;subset=latin-ext" rel="stylesheet">
-		 <link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap.min.css"/>
-
-
-
-<link rel="stylesheet" type="text/css" href="DataTables/datatables.min.css"/>
-
-<script type="text/javascript" src="DataTables/datatables.min.js"></script>
-<script src="js/jquery.js"></script>
-
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-</head>
     <body>
+     <!-- start topBar -->
 
-        <!-- start topBar -->
-       <?php require 'topbar.php'; ?>
-	      <?php require 'middlebar.php'; ?>
-		     <?php require 'navh.php'; ?>
-        <!-- end topBar -->
-
-        <!-- start navbar -->
+     <?php include('topbar.php');
+	include('middlebar.php');
+	  include('navh.php');
+	   ?>
 
 
 
