4c457a16a7be66a72818ed8a89c6fc7dcb733013
Jose Ortega <mauricio1314@hotmail.com>
2018-03-25 16:45:58 -0400

    Detalles

--- webpay/sample/index.php
+++ webpay/sample/index.php
@@ -1,9 +1,14 @@
+<?php session_start();
+$total = $_GET['total'];
+
+?>
+
 <h1>Ejemplos Webpay</h1>
 
 <table border="0" style="width:70%">
   <tr>
     <td><h3>Transacci&oacute;n Normal</h3></td>
-    <td><h3><a href="tbk-normal.php">webpay Normal</a></h3></td> 
+    <td><h3><a href="tbk-normal.php?total=<?php echo $total?>">webpay Normal</a></h3></td> 
     <td><h3><h3><a href="tbk-nullify-normal.php">webpay Normal Anulaci&oacute;n </a></h3></h3></td>
   </tr>
   <tr>
